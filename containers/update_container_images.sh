e3af8df8201d97ac438edb2a1385f4b35210b096
Víctor Gibrán Moreno Zárate <victorgibranmz@hotmail.com>
2021-10-23 18:29:51 -0500

    Update update_container_images.sh

--- containers/update_container_images.sh
+++ containers/update_container_images.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-for d in ./*/ ; 
-    do (cd "$d" && docker-compose pull && docker-compose up -d); 
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
+for d in ./repo/containers/*/ ;
+    do (cd "$d" && docker-compose pull && docker-compose up -d);
 done
 docker image prune -f
 docker builder prune -f
